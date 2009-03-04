3ffa19abe5bd98e3fc7fee1b61133a00a6e4da12
Dmitry E. Oboukhov <unera@debian.org>
2009-03-04 13:35:17 +0000

    Import of UNERA/RTPG-0.2.0 from CPAN.
    gitpan-cpan-distribution: RTPG
    gitpan-cpan-version:      0.2.0
    gitpan-cpan-path:         UNERA/RTPG-0.2.0.tar.gz
    gitpan-cpan-author:       UNERA
    gitpan-cpan-maturity:     released

--- RTPG.pm
+++ RTPG.pm
@@ -23,11 +23,11 @@ RTPG - is a module for accessing to rtorrent's SCGI functions.
 
 =head1 VERSION
 
-0.1.1
+0.2.0
 
 =cut
 
-our $VERSION="0.1.1";
+our $VERSION="0.2.0";
 
 =head1 SYNOPSIS
 
--- RTPG/Direct.pm
+++ RTPG/Direct.pm
@@ -9,11 +9,11 @@ RTPG::Direct - is a driver for L<RTPG>.
 
 =head1 VERSION
 
-0.1.1
+0.2.0
 
 =cut
 
-our $VERSION="0.1.1"
+our $VERSION="0.2.0";
 
 =head1 SYNOPSIS
 
